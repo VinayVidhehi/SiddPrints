3685aedff6fa88078d2ba31d4be1a4179a2b5d9c
VinayVidhehi <vinayvidhehi@gmail.com>
2023-10-14 10:04:32 +0530

    updated backend for azure storage

--- app.js
+++ app.js
@@ -5,7 +5,19 @@ const cors = require('cors');
 const multer = require("multer");
 const path = require("path");
 const File = require("./schema");
-const User = require("./UserSchema")
+const User = require("./UserSchema");
+const fs = require("fs"); // Added this line to use 'fs'
+
+const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob");
+
+const accountName = "siddvinay";
+const accountKey = "9Zct7vAKKhv4bgztNT+b7aU3FpXpEkkkKga9Kuvh9NbrtJXGg3Brzo79DXPlfRDOh47OmwGGOfRq+AStXGNVHw==";
+
+const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey);
+const blobServiceClient = new BlobServiceClient(`https://${accountName}.blob.core.windows.net`, sharedKeyCredential);
+
+const containerName = "uploads"; // Replace with your container name
+const containerClient = blobServiceClient.getContainerClient(containerName);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,7 +29,7 @@ mongoose
   .connect("mongodb+srv://alpha:P9UlObl4ugVQx8JF@cluster0.gbppnze.mongodb.net/3d", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
+  });
 
 let filename;
 const storage = multer.diskStorage({
@@ -38,12 +50,29 @@ app.post("/api/upload", upload.single("file"), async (req, res) => {
       return res.status(400).json({ message: "No file uploaded" });
     }
 
-   const uploadfile = new File({
-    useremail : req.body.email,
-    filename : filename,
-    path : req.file.path,
-  });
+    console.log(req.body.email);
+    const useremail = req.body.email;
+    const localFilePath = req.file.path;
 
+    // Use the Azure Storage SDK to upload the file to Azure Blob Storage
+    const blobName = Date.now() + "-" + req.file.originalname;
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+    await blockBlobClient.uploadFile(localFilePath);
+
+    console.log(localFilePath);
+
+    // Clean up the local file after uploading to Azure
+    fs.unlinkSync(localFilePath);
+
+    const fileUrl = blockBlobClient.url;
+    
+    // Save the file information (e.g., useremail, fileUrl) to your MongoDB database
+    const uploadfile = new File({
+      useremail: useremail,
+      filename: blobName,
+      path: fileUrl,
+    });
 
     await uploadfile.save();
     res.json({ message: "File uploaded successfully" });
@@ -81,22 +110,20 @@ app.post("/api/signup", async (req, res) => {
 
   if(finduser){
     if(finduser.username === username) {
-      console.log("here??")
+      console.log("here??");
       res.status(201).json({message:"user exists"});
     }
-  }
-
-  else {
-    const updateUser = new User ({
+  } else {
+    const updateUser = new User({
       username,
       email,
       password,
-   })
+    });
  
-   await updateUser.save();
-   res.status(200).json({message:"user created successfully. refresh page and please login"})
+    await updateUser.save();
+    res.status(200).json({message:"user created successfully. Refresh the page and please login"});
   }
-  }catch (error) {
+  } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
   }
@@ -104,4 +131,6 @@ app.post("/api/signup", async (req, res) => {
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
